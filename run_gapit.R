428ca82281df1d09d7d4b383df95e8a7ae7a2a0d
udiland <udiland@tauex.tau.ac.il>
2022-10-24 16:55:40 +0300

    first_commit

---
+++ run_gapit.R
@@ -0,0 +1,21 @@
+# Get GAPIT
+source("/home/username/GAPIT.library.R")
+source("/home/username/gapit_functions.txt")
+
+# upload phenotype file
+myY <- read.table("/home/username/../phenotype.tsv", sep='\t', head=T)
+
+myY[,2] <- as.numeric(myY[,2])
+
+
+# Read genomic data - this will load the LD fiiltered file with snps in all chromosomes
+myG <- read.table("/home/udiland/gwas/snp/data/geno/all_samples/LD_pruned_100/long_all_pruned.hmp.txt", sep='\t', comment.char = "", head=F)
+
+
+# Run GAPIT3
+myGAPIT <- GAPIT(
+  Y=myY,
+  PCA.total=2,
+  G=myG,
+  model=c("GLM","MLM", "MLMM", "FarmCPU", "Blink")
+)
